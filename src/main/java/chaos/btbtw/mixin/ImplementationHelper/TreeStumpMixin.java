9fe89b804b873c2c27fd7f66f5ef85ab3f0b492b
HerrChaos <cedrik.zwick@gmx.net>
2024-03-02 12:40:50 +0100

    made dark oak tree stump gen work

--- src/main/java/chaos/btbtw/mixin/ImplementationHelper/TreeStumpMixin.java
+++ src/main/java/chaos/btbtw/mixin/ImplementationHelper/TreeStumpMixin.java
@@ -10,6 +10,7 @@ import net.minecraft.world.StructureWorldAccess;
 import net.minecraft.world.gen.feature.TreeFeature;
 import net.minecraft.world.gen.feature.TreeFeatureConfig;
 import net.minecraft.world.gen.foliage.FoliagePlacer;
+import net.minecraft.world.gen.trunk.DarkOakTrunkPlacer;
 import net.minecraft.world.gen.trunk.GiantTrunkPlacer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -23,8 +24,6 @@ public abstract class TreeStumpMixin {
 	@Inject(at = @At("RETURN"), method = "generate(Lnet/minecraft/world/StructureWorldAccess;Lnet/minecraft/util/math/random/Random;Lnet/minecraft/util/math/BlockPos;Ljava/util/function/BiConsumer;Ljava/util/function/BiConsumer;Lnet/minecraft/world/gen/foliage/FoliagePlacer$BlockPlacer;Lnet/minecraft/world/gen/feature/TreeFeatureConfig;)Z")
 	public void generate(StructureWorldAccess world, Random random, BlockPos pos, BiConsumer<BlockPos, BlockState> rootPlacerReplacer, BiConsumer<BlockPos, BlockState> trunkPlacerReplacer, FoliagePlacer.BlockPlacer blockPlacer, TreeFeatureConfig config, CallbackInfoReturnable<Boolean> cir) {
 		if (cir.getReturnValue()) {
-			System.out.println(config.trunkProvider.get(random, pos).getBlock().getTranslationKey());
-
 			CompareList LogBlocks = new CompareList();
 			LogBlocks.add("block.minecraft.oak_log", ModBlocks.TREE_STUMP_OAK, null);
 			LogBlocks.add("block.minecraft.birch_log", ModBlocks.TREE_STUMP_BIRCH, null);
@@ -34,8 +33,7 @@ public abstract class TreeStumpMixin {
 			LogBlocks.add("block.minecraft.dark_oak_log", ModBlocks.TREE_STUMP_DARK_OAK, null);
 
 			blockPlacer.placeBlock(pos, ((Block) LogBlocks.checkFor(config.trunkProvider.get(random, pos).getBlock().getTranslationKey())).getDefaultState());
-			if (config.trunkPlacer instanceof GiantTrunkPlacer) {
-				//TODO Dark oak not working
+			if ((config.trunkPlacer instanceof GiantTrunkPlacer) || (config.trunkPlacer instanceof DarkOakTrunkPlacer)) {
 				blockPlacer.placeBlock(pos.add(1,0,0), ((Block) LogBlocks.checkFor(config.trunkProvider.get(random, pos).getBlock().getTranslationKey())).getDefaultState());
 				blockPlacer.placeBlock(pos.add(1,0,1), ((Block) LogBlocks.checkFor(config.trunkProvider.get(random, pos).getBlock().getTranslationKey())).getDefaultState());
 				blockPlacer.placeBlock(pos.add(0,0,1), ((Block) LogBlocks.checkFor(config.trunkProvider.get(random, pos).getBlock().getTranslationKey())).getDefaultState());
